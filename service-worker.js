aa8a65965330343fc1815e8cfa2b4e868c11160e
Fandom-Entry-pass <alexisdeshong@gfcleaners.com>
2025-09-02 23:55:30 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE = "fep-cache-v2";
+const CACHE = "fep-cache-v3";
 const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
