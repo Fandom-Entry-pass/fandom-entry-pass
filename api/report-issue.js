d01d3d57a6690d44278b34187bba5948133c40d1
Fandom-Entry-pass <alexisdeshong@gfcleaners.com>
2025-09-16 09:24:36 -0400

    Update report-issue.js

--- api/report-issue.js
+++ api/report-issue.js
@@ -1,7 +1,7 @@
 // api/report-issue.js
 import Stripe from "stripe";
 
-export const config = { runtime: "nodejs18.x" };
+export const config = { runtime: "nodejs" };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: "2024-06-20" });
 
