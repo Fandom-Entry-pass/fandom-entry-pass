cfad82507726e32b63ce09224fefcd07274da962
Fandom-Entry-pass <alexisdeshong@gfcleaners.com>
2025-09-21 15:17:50 -0400

    Update config.js
    EMAILJS_TPL_SELLER is now your one flexible template that works for seller notices, buyer notices, and login codes.
    
    If you hit template limits, you can delete EMAILJS_TPL_BUYER entirely and just reference EMAILJS_TPL_SELLER everywhere.

--- config.js
+++ config.js
@@ -1,10 +1,19 @@
 window.APP_CONFIG = {
   FORMSPREE: "https://formspree.io/f/mvgqedqo",
+
+  // EmailJS
   EMAILJS_PUBLIC: "PioDqOAQEpgJXFr7G",
   EMAILJS_SERVICE: "service_FEP0",
-  EMAILJS_TPL_SELLER: "template_SellerNots",
-  EMAILJS_TPL_BUYER: "template_BuyerNots",
+  EMAILJS_TPL_SELLER: "template_SellerNots", // unified template (seller, buyer, login)
+  EMAILJS_TPL_BUYER: "template_BuyerNots",   // optional (you can delete if not needed)
+
+  // Backend
   API_BASE: "", // empty string defaults to same-origin
+
+  // Stripe
   STRIPE_PK: "pk_live_51LcNBuF7BMVtRlnacvmpKOmS9gMBg3IOnkdgaOjdRjCCspQNjHuvPBwLXBdxIn2qC0bJpa1yO2GZjaTbMOvwvr7n00sb6AKo1u",
-  ESCROW_HOURS: 72,
+
+  // Escrow
+  ESCROW_HOURS: 72
 };
+
